2416d7a4fa12e7f926a5d192d10d5c34dcc8c1b8
Danison dos Santos <danison.santos00@gmail.com>
2023-10-29 20:06:06 -0300

    crete project

---
+++ src/main/java/com/dan/StudentDwellMate/StudentDwellMateApplication.java
@@ -0,0 +1,13 @@
+package com.dan.StudentDwellMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDwellMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDwellMateApplication.class, args);
+	}
+
+}
