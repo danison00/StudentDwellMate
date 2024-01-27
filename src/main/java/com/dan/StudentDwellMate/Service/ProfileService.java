a0b311d698e324b48e667127b4a17230e518f9d1
Danison dos Santos <danison.santos00@gmail.com>
2024-01-27 12:58:54 -0300

    add profile service

---
+++ src/main/java/com/dan/StudentDwellMate/Service/ProfileService.java
@@ -0,0 +1,8 @@
+package com.dan.StudentDwellMate.Service;
+
+import com.dan.StudentDwellMate.model.dto.ProfileDto;
+
+public interface ProfileService {
+    
+    void save(ProfileDto profileDto);
+}
