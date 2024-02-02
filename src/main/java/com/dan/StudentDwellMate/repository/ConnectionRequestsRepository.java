558da24dc73de4be7cdfc343f8f2418437e7ac8d
Danison dos Santos <danison.santos00@gmail.com>
2024-02-01 22:05:37 -0300

    refact: repository

---
+++ src/main/java/com/dan/StudentDwellMate/repository/ConnectionRequestsRepository.java
@@ -0,0 +1,19 @@
+package com.dan.StudentDwellMate.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.dan.StudentDwellMate.model.entities.ConnectionRequest;
+import com.dan.StudentDwellMate.model.entities.Profile;
+
+public interface ConnectionRequestsRepository extends JpaRepository<ConnectionRequest, Long>{
+    
+
+    @Query("SELECT cr FROM ConnectionRequest cr WHERE (cr.sender.id = :idSender AND cr.receiver.id = :idReceiver) OR (cr.receiver.id = :idSender AND cr.sender.id = :idReceiver)")
+    Optional<ConnectionRequest> connectionRequestAlreadyExists(Long idSender, Long idReceiver);
+
+
+}
