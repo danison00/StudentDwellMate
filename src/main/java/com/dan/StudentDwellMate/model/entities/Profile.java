c9b049043bee88708bf34526da0afff2bc667f13
Danison dos Santos <danison.santos00@gmail.com>
2024-02-01 22:05:09 -0300

    refact: domain

--- src/main/java/com/dan/StudentDwellMate/model/entities/Profile.java
+++ src/main/java/com/dan/StudentDwellMate/model/entities/Profile.java
@@ -2,6 +2,7 @@ package com.dan.StudentDwellMate.model.entities;
 
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -25,7 +26,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -55,9 +55,11 @@ public class Profile implements Serializable{
         @OneToOne(mappedBy = "profile", cascade = CascadeType.ALL)
         private Property property;
 
-        @ManyToMany(cascade = CascadeType.ALL)
-        @JoinTable(name = "connection_requests", joinColumns = @JoinColumn(name = "id_fk_sender_profile"), inverseJoinColumns = @JoinColumn(name = "id_fk_receiver_profile"))
-        private Set<Profile> connectionRequest;
+        @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL)
+        private List<ConnectionRequest> resquestsConnectionSent;
+        
+        @OneToMany(mappedBy = "receiver", cascade = CascadeType.ALL)
+        private List<ConnectionRequest> resquestsConnectionReceived;
 
         @ManyToMany(cascade = CascadeType.ALL)
         @JoinTable(name = "connections", joinColumns = @JoinColumn(name = "id_fk_profile"), inverseJoinColumns = @JoinColumn(name = "id_fk_profile_connected"))
@@ -85,5 +87,102 @@ public class Profile implements Serializable{
                 this.wantsToSharedProperty = wantsToSharedProperty;
                 this.property = property;
         }
+
+        @Override
+        public int hashCode() {
+                final int prime = 31;
+                int result = 1;
+                result = prime * result + ((id == null) ? 0 : id.hashCode());
+                result = prime * result + ((name == null) ? 0 : name.hashCode());
+                result = prime * result + ((faculty == null) ? 0 : faculty.hashCode());
+                result = prime * result + ((course == null) ? 0 : course.hashCode());
+                result = prime * result + age;
+                result = prime * result + ((email == null) ? 0 : email.hashCode());
+                result = prime * result + ((instagram == null) ? 0 : instagram.hashCode());
+                result = prime * result + ((facebook == null) ? 0 : facebook.hashCode());
+                result = prime * result + ((whatsapp == null) ? 0 : whatsapp.hashCode());
+                result = prime * result + ((profilePhotoUrl == null) ? 0 : profilePhotoUrl.hashCode());
+                result = prime * result + (hasRentedProperty ? 1231 : 1237);
+                result = prime * result + ((cityOrigin == null) ? 0 : cityOrigin.hashCode());
+                result = prime * result + (wantsToSharedProperty ? 1231 : 1237);
+                result = prime * result + ((property == null) ? 0 : property.hashCode());
+                return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+                if (this == obj)
+                        return true;
+                if (obj == null)
+                        return false;
+                if (getClass() != obj.getClass())
+                        return false;
+                Profile other = (Profile) obj;
+                if (id == null) {
+                        if (other.id != null)
+                                return false;
+                } else if (!id.equals(other.id))
+                        return false;
+                if (name == null) {
+                        if (other.name != null)
+                                return false;
+                } else if (!name.equals(other.name))
+                        return false;
+                if (faculty == null) {
+                        if (other.faculty != null)
+                                return false;
+                } else if (!faculty.equals(other.faculty))
+                        return false;
+                if (course == null) {
+                        if (other.course != null)
+                                return false;
+                } else if (!course.equals(other.course))
+                        return false;
+                if (age != other.age)
+                        return false;
+                if (email == null) {
+                        if (other.email != null)
+                                return false;
+                } else if (!email.equals(other.email))
+                        return false;
+                if (instagram == null) {
+                        if (other.instagram != null)
+                                return false;
+                } else if (!instagram.equals(other.instagram))
+                        return false;
+                if (facebook == null) {
+                        if (other.facebook != null)
+                                return false;
+                } else if (!facebook.equals(other.facebook))
+                        return false;
+                if (whatsapp == null) {
+                        if (other.whatsapp != null)
+                                return false;
+                } else if (!whatsapp.equals(other.whatsapp))
+                        return false;
+                if (profilePhotoUrl == null) {
+                        if (other.profilePhotoUrl != null)
+                                return false;
+                } else if (!profilePhotoUrl.equals(other.profilePhotoUrl))
+                        return false;
+                if (hasRentedProperty != other.hasRentedProperty)
+                        return false;
+                if (cityOrigin == null) {
+                        if (other.cityOrigin != null)
+                                return false;
+                } else if (!cityOrigin.equals(other.cityOrigin))
+                        return false;
+                if (wantsToSharedProperty != other.wantsToSharedProperty)
+                        return false;
+                if (property == null) {
+                        if (other.property != null)
+                                return false;
+                } else if (!property.equals(other.property))
+                        return false;
+                return true;
+        }
+
+       
+        
 
 }
--- src/main/java/com/dan/StudentDwellMate/model/entities/Property.java
+++ src/main/java/com/dan/StudentDwellMate/model/entities/Property.java
@@ -7,9 +7,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data @Builder @NoArgsConstructor @AllArgsConstructor
+
+
+@Builder @NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
 @Entity
 @Table
 public class Property implements Serializable{
@@ -31,6 +36,81 @@ public class Property implements Serializable{
     private String postalCode;
 
     List<String> propertyPhoto;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((city == null) ? 0 : city.hashCode());
+        result = prime * result + ((state == null) ? 0 : state.hashCode());
+        result = prime * result + ((street == null) ? 0 : street.hashCode());
+        result = prime * result + ((neighborhood == null) ? 0 : neighborhood.hashCode());
+        result = prime * result + ((houseNumber == null) ? 0 : houseNumber.hashCode());
+        result = prime * result + ((additionalDetails == null) ? 0 : additionalDetails.hashCode());
+        result = prime * result + ((postalCode == null) ? 0 : postalCode.hashCode());
+        result = prime * result + ((propertyPhoto == null) ? 0 : propertyPhoto.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Property other = (Property) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (city == null) {
+            if (other.city != null)
+                return false;
+        } else if (!city.equals(other.city))
+            return false;
+        if (state == null) {
+            if (other.state != null)
+                return false;
+        } else if (!state.equals(other.state))
+            return false;
+        if (street == null) {
+            if (other.street != null)
+                return false;
+        } else if (!street.equals(other.street))
+            return false;
+        if (neighborhood == null) {
+            if (other.neighborhood != null)
+                return false;
+        } else if (!neighborhood.equals(other.neighborhood))
+            return false;
+        if (houseNumber == null) {
+            if (other.houseNumber != null)
+                return false;
+        } else if (!houseNumber.equals(other.houseNumber))
+            return false;
+        if (additionalDetails == null) {
+            if (other.additionalDetails != null)
+                return false;
+        } else if (!additionalDetails.equals(other.additionalDetails))
+            return false;
+        if (postalCode == null) {
+            if (other.postalCode != null)
+                return false;
+        } else if (!postalCode.equals(other.postalCode))
+            return false;
+        if (propertyPhoto == null) {
+            if (other.propertyPhoto != null)
+                return false;
+        } else if (!propertyPhoto.equals(other.propertyPhoto))
+            return false;
+        return true;
+    }
+
+
     
 }
 
