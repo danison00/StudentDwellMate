926f74f6b014b0b4e6f241d4dbb05c10c79c44b2
Danison dos Santos <danison.santos00@gmail.com>
2024-02-02 19:04:17 -0300

    refact: remove not responsabilities

--- src/main/java/com/dan/StudentDwellMate/Service/Impl/ProfileServiceImpl.java
+++ src/main/java/com/dan/StudentDwellMate/Service/Impl/ProfileServiceImpl.java
@@ -1,20 +1,13 @@
 package com.dan.StudentDwellMate.Service.Impl;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.dan.StudentDwellMate.Service.ProfileService;
 import com.dan.StudentDwellMate.model.dto.ProfileRequestDto;
 import com.dan.StudentDwellMate.model.dto.response.ProfileResponseDto;
-import com.dan.StudentDwellMate.model.entities.ConnectionRequest;
 import com.dan.StudentDwellMate.model.entities.Profile;
-import com.dan.StudentDwellMate.repository.ConnectionRequestsRepository;
 import com.dan.StudentDwellMate.repository.ProfileRepository;
 import com.dan.StudentDwellMate.util.Mapper;
 
@@ -23,6 +16,8 @@ public class ProfileServiceImpl implements ProfileService {
 
     @Autowired
     private ProfileRepository profileRep;
+
+
 
     @Override
     public void save(ProfileRequestDto profileDto) throws DataIntegrityViolationException {
@@ -43,23 +38,9 @@ public class ProfileServiceImpl implements ProfileService {
         return profilesDto;
     }
 
-    @Override
-    public List<ProfileResponseDto> getProfilesFromConnectionRequestsSent(Long idProfile) {
-
-        this.findById(idProfile);        
-
-        return Mapper.getProfileDto(this.profileRep.getProfilesFromConnectionRequestsSent(idProfile));
-    }
-
   
 
-    @Override
-    public List<ProfileResponseDto> getProfilesFromConnectionRequestsReceiver(Long idProfile) {
-   
-        this.findById(idProfile);        
 
-        return Mapper.getProfileDto(this.profileRep.getProfilesFromConnectionRequestsReceiver(idProfile));
-    }
     @Override
     public Profile findById(Long id) {
         return this.profileRep.findById(id).orElseThrow(()-> new RuntimeException("Usuário não encontrado"));
--- src/main/java/com/dan/StudentDwellMate/Service/ProfileService.java
+++ src/main/java/com/dan/StudentDwellMate/Service/ProfileService.java
@@ -1,14 +1,10 @@
 package com.dan.StudentDwellMate.Service;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.dao.DataIntegrityViolationException;
-
 import com.dan.StudentDwellMate.model.dto.ProfileRequestDto;
 import com.dan.StudentDwellMate.model.dto.response.ProfileResponseDto;
 import com.dan.StudentDwellMate.model.entities.Profile;
-
 public interface ProfileService {
 
     void save(ProfileRequestDto profileDto) throws DataIntegrityViolationException;
@@ -18,10 +14,6 @@ public interface ProfileService {
     boolean existsByEmail(String email);
 
     List<ProfileResponseDto> getAllProfiles(Long id);
-
-    List<ProfileResponseDto> getProfilesFromConnectionRequestsSent(Long idProfile);
-
-    List<ProfileResponseDto> getProfilesFromConnectionRequestsReceiver(Long idProfile);
 
 
 }
